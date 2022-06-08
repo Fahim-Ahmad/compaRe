f5aea5cf276a99e5dcf527dd91b03dadebcb6366
atrcodebase <39668413+Fahim-Ahmad@users.noreply.github.com>
2022-06-09 01:22:03 +0430

    added 'compare_all' argument

--- R/compare_df.R
+++ R/compare_df.R
@@ -6,6 +6,7 @@
 #' @param df2 Latest version of the dataset
 #' @param unique_id_df1 unique identifier in df1
 #' @param unique_id_df2 unique identifier in df2
+#' @param compare_all logical. `TRUE`: compare all columns/variables. `FALSE`: compare only shared columns/variable.
 #' @examples
 #' library(compaRe)
 #'
@@ -16,20 +17,33 @@
 #' mtcars_v2$wt[mtcars_v2$wt > 100] <- 105
 #' mtcars_v2$cyl[mtcars_v2$cyl == 8] <- 7
 #' mtcars_v2 <- mtcars_v2[1:30, ]
+#' mtcars_v2$rowid <- 1:nrow(mtcars_v2)
 #'
 #' compare_df(df1 = mtcars1, df2 = mtcars2,
 #'           unique_id_df1 = "id",
-#'           unique_id_df2 = "id"
+#'           unique_id_df2 = "id",
+#'           compare_all = TRUE
+#' )
+#'
+#' compare_df(df1 = mtcars1, df2 = mtcars2,
+#'           unique_id_df1 = "id",
+#'           unique_id_df2 = "id",
+#'           compare_all = FALSE
 #' )
 #' @usage
 #' library(compaRe)
-#' compare_df(df1, df2, unique_id_df1, unique_id_df2)
+#' compare_df(df1, df2, unique_id_df1, unique_id_df2, compare_all = TRUE)
 
 #' @import tidyr
 #' @import dplyr
 #' @import stringr
 #' @export
-compare_df <- function(df1, df2, unique_id_df1, unique_id_df2) {
+compare_df <- function(df1, df2, unique_id_df1, unique_id_df2, compare_all = TRUE) {
+
+  if(compare_all == FALSE) {
+    df1 <- df1[, colnames(df1) %in% colnames(df2)]
+    df2 <- df2[, colnames(df2) %in% colnames(df1)]
+  }
 
   if ("KEY" %in% colnames(df1) & unique_id_df1 != "KEY") {
     df1 <- df1 %>%
--- README.md
+++ README.md
@@ -18,7 +18,7 @@ install_github("Fahim-Ahmad/compaRe")
 ## Usage
 ``` r
 library(compaRe)
-compare_df(df1, df2, unique_id_df1, unique_id_df2)
+compare_df(df1, df2, unique_id_df1, unique_id_df2, compare_all = TRUE)
 ```
 
 ## Arguments
@@ -29,6 +29,8 @@ compare_df(df1, df2, unique_id_df1, unique_id_df2)
   `unique_id_df1` unique identifier in df1
 
   `unique_id_df2` unique identifier in df2
+  
+  `compare_all` logical. `TRUE`: compare all columns/variables. `FALSE`: compare only shared columns/variable. 
 
 ## Example
 
@@ -42,10 +44,18 @@ mtcars_v2$am[mtcars_v2$am == 1] <- 2
 mtcars_v2$wt[mtcars_v2$wt > 100] <- 105
 mtcars_v2$cyl[mtcars_v2$cyl == 8] <- 7
 mtcars_v2 <- mtcars_v2[1:30, ]
+mtcars_v2$rowid <- 1:nrow(mtcars_v2)
 
-compare_df(df1 = mtcars1, df2 = mtcars2,
+compare_df(df1 = mtcars_v1, df2 = mtcars_v2,
           unique_id_df1 = "id",
-          unique_id_df2 = "id"
+          unique_id_df2 = "id",
+          compare_all = TRUE
+          )
+          
+compare_df(df1 = mtcars_v1, df2 = mtcars_v2,
+          unique_id_df1 = "id",
+          unique_id_df2 = "id",
+          compare_all = FALSE
           )
 ```
 
